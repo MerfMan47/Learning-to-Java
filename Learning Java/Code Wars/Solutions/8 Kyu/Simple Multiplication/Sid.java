7685c10a309a8d24221bba907167dab50912189d
JR McGregor <43829830+MerfMan47@users.noreply.github.com>
2023-03-19 22:48:39 -0700

    Add files via upload

---
+++ Learning Java/Code Wars/Solutions/8 Kyu/Simple Multiplication/Sid.java
@@ -0,0 +1,16 @@
+
+/**
+ * First test with Java from a Codewars problem
+ *
+ * @author Joshua McGregor
+ * @version 3/19/2023
+ */
+public class Sid {
+    public static int simpleMultiplication(int n) {
+        if(n%2 == 0){
+          return n*8;
+        }else{
+          return n*9;
+        }
+    }
+}
